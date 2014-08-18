533cc278fc427f5476d95d0d087039fc9c4b0dca
felo <felipe.longe@outlook.com>
2014-08-18 12:26:14 +0200

    init

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+
+/*
+ * GET home page.
+ */
+
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.json(
+        {
+            data : "test"
+        }
+    );
+
+});
+
+module.exports = router;
