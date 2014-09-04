f227f13ba4bedd1d79a55f495d4b9260472d41dd
Jørn Kinderås <jorn.kinderas@makingwaves.no>
2014-09-04 10:19:08 +0200

    Fixes the PDL cacher to support logged in users and different chains

--- modules/productDetails_caching/pdl_cacher.js
+++ modules/productDetails_caching/pdl_cacher.js
@@ -3,14 +3,20 @@
 // This will cache ProductDetailList calls for users whom are not logged in
 
 var request = require('request');
+var hash = require('hash-string');
 var basicToken = 'Basic J8ed0(tyAop206%JHP';
-var pdlCache;
-var cacheTime;
+var pdlCache = [];
 var ONE_HOUR = 60 * 60 * 1000;
 
-function fetchProductDetails(requestBody, callback) {
+var hashObject = {
+	key       : "",
+	cacheTime : {},
+	data      : {}
+};
+
+function fetchProductDetails (requestBody, cacheObj, callback) {
 	var uri = "";
-	if(requestBody.environment === 'production') {
+	if (requestBody.environment === 'production') {
 		uri = "https://service-dk.norgesgruppen.no/" + requestBody.servicepath;
 	} else {
 		uri = "https://preprod.service-dk.norgesgruppen.no/" + requestBody.servicepath;
@@ -23,46 +29,74 @@ function fetchProductDetails(requestBody, callback) {
 		headers[h[0]] = header[h[0]];
 	});
 	var options = {
-		url: uri,
-		headers: headers,
-		method: 'POST',
-		json: requestBody.payload
+		url     : uri,
+		headers : headers,
+		method  : 'POST',
+		json    : requestBody.payload
 	};
 
-	request(options, function(error, response, body) {
-		if(error) {
-			callback(null, {error: "Server malfunction"});
+	request(options, function (error, response, body) {
+		if (error) {
+			callback(null, {error : "Server malfunction"});
 		} else {
-			if(response.statusCode === 200) {
-				pdlCache = body;
-				cacheTime = Date.now();
+			if (response.statusCode === 200) {
+				cacheObj.data = body;
+				cacheObj.cacheTime = Date.now();
+
+				if (cacheObj.isNew) {
+					cacheObj.isNew = false;
+					pdlCache.push(cacheObj);
+				}
+
 				callback(body, null);
 			} else {
-				callback(null, {error: body});
+				callback(null, {error : body});
 			}
 		}
-	});
+	}.bind(this));
 }
 
-function old () {
-	return (Date.now() - cacheTime) > ONE_HOUR;
+function old (time) {
+	return (Date.now() - time) > ONE_HOUR;
+}
+
+function getCacheByKey (key) {
+	var i = pdlCache.length;
+
+	while (i--) {
+		if (pdlCache[i].key === key) {
+			return pdlCache[i];
+		}
+	}
+
+	return false;
 }
 
 exports.fetch = function (requestBody, callback) {
-	var requestToken;
+	var requestToken, hashKey, cacheObj;
 
-	requestBody.headers.forEach(function(header) {
-		if(header.Authorization) {
+	requestToken = basicToken;
+	requestBody.headers.forEach(function (header) {
+		if (header.Authorization) {
 			requestToken = header.Authorization;
 		}
 	});
 
-	if (!pdlCache || old() || (requestToken !== basicToken)) {
-		fetchProductDetails(requestBody, function (response, error) {
-			callback(response, error);
-		});
+	hashKey = hash.hashCode(requestBody.servicepath + requestToken);
+	cacheObj = getCacheByKey(hashKey);
+
+	if (cacheObj && !old(cacheObj.cacheTime)) {
+		callback(cacheObj.data, null);
 	}
 	else {
-		callback(pdlCache, null);
+		cacheObj = {
+			key       : hashKey,
+			cacheTime : null,
+			data      : null,
+			isNew     : true
+		};
+		fetchProductDetails(requestBody, cacheObj, function (response, error) {
+			callback(response, error);
+		});
 	}
 };
