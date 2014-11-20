3b701cb889df146363ff6f4138a7f3a096871b7e
Pal Ronning <pronning@gmail.com>
2014-11-20 15:30:31 +0100

    Refactoring, debugging and making the tests run smoothly again

--- modules/stores/store_service.js
+++ modules/stores/store_service.js
@@ -4,42 +4,63 @@
 
 var geolib = require('geolib');
 var moment = require('moment-timezone');
-
+var util = require('./store-utils');
 var repository;
 
 var inMemRepo = {
-    getStores: function repository(cb) {
-        cb(require('./data/kiwistores'));
+    getStores: function repository(chainId, cb) {
+        cb(require('../../spec/stores/fixtures/kiwistores'));
     }
 };
 
 repository = require('./store-repository.js');
+//repository = inMemRepo; 
 
-function isNumber(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
 
-function filterByLimits(storeArray, minNumberOfStores, maxNumberOfStores, maxDistance) {
 
-    if (maxDistance === 0 && maxNumberOfStores === 0 || minNumberOfStores === storeArray.length) {
-        return storeArray;
-    }
+// TODO: parametres as object
+function getClosestStores(chainId, latitude, longitude, minNumberOfStores, maxNumberOfStores, maxDistance, filter, callback) {
+    var myPos = { "latitude": latitude, "longitude": longitude };
 
-    for (var i = minNumberOfStores; i < storeArray.length; i++) {
+    // TODO: lage en var av validert stores
+    repository.getStores(chainId, function(stores) {
 
-        // frontend expects distance in kilometers, in spite of passing metres in
-        if (maxDistance !== 0 && storeArray[i].distance * 1000 > maxDistance) {
-            return storeArray.slice(0, i);
+        if (stores.response.code != "200") {
+            throw new Error("Service returned... ");
         }
 
-        // using '+' ahead of 'i' in order to avoid string concat instead of sum
-        if (maxNumberOfStores !== 0 && ( +i + 1 - minNumberOfStores) >= maxNumberOfStores) {
-            return storeArray.slice(0, i);
+        var i = stores.response.data.length;
+        var storeArray = [];
+        while (i--) {
+            var store = stores.response.data[i];
+            var distance,
+                location = store.location;
+
+            var haslocation = util.isValidLocation(location);
+            if (!haslocation)
+                continue;
+
+            distance = geolib.getDistance(myPos, location) / 1000;
+            storeArray.push(createStoreDistanceObject(store, distance));
+
         }
 
-    }
+        if (storeArray.length > 0) {
 
-    return storeArray;
+            storeArray.sort(function(a, b) {
+                return a.distance - b.distance;
+            });
+
+            storeArray = util.filterByOpeninghours(storeArray, filter);
+            storeArray = util.filterByLimits(storeArray, minNumberOfStores, maxNumberOfStores, maxDistance);
+            storeArray = util.limitNumberOfSpecialOpeningHoursAhead(storeArray, 7);
+
+            var today = moment().tz("Europe/Oslo");
+            util.applyTodaysOpeningHours(today, storeArray);
+        }
+        if (callback)
+            callback(storeArray);
+    });
 }
 
 function createStoreDistanceObject(storeObject, distance) {
@@ -49,152 +70,7 @@ function createStoreDistanceObject(storeObject, distance) {
     };
 }
 
-function filterOnOpenLate(storeArray) {
-
-    return storeArray.filter(function (elem) {
-
-        if (elem && elem.store && elem.store.openinghours) {
-            return elem.store.openinghours.isopenlate;
-        }
-
-        return false;
-    });
-}
-
-function filterOnOpenSunday(storeArray) {
-    return storeArray.filter(function (elem) {
-        if (elem && elem.store && elem.store.openinghours) {
-            return elem.store.openinghours.isopenonsunday;
-        }
-        return false;
-    });
-}
-
-
-function filterByOpeninghours(storeArray, filter) {
-    if (filter) {
-        if (filter.toLowerCase() === "isopenlate") {
-            return filterOnOpenLate(storeArray);
-        }
-
-        if (filter.toLowerCase() === "isopensunday") {
-            return filterOnOpenSunday(storeArray);
-        }
-    }
-    return storeArray;
-}
-
-// CODE REVIEW PLEASE
-function applyTodaysOpeningHours(today, stores) {
-    
-    stores.map(function (elem) {
-        
-        if (elem && elem.store && elem.store.openinghours && elem.store.openinghours.today) {
-            
-            // if cached version is up to date (!), no worries
-            if (elem.store.openinghours.today && elem.store.openinghours.today.date == today.format("YYYY-MM-DD"))
-                return;
-            }
-
-            elem.store.openinghours.today.from = '';
-            elem.store.openinghours.today.to = '';
-
-            // if not, overwrite with special openinghours, if any
-            if (elem.store.openinghours.special) {
-                elem.store.openinghours.special.map(function (special) {
-                    if (special.date == today.format("YYYY-MM-DD")) {
-                        elem.store.openinghours.today.from = special.from;
-                        elem.store.openinghours.today.to = special.to;
-                    }
-                    return;
-                });
-            }
-            
-        // if not, apply ordinary weekday rules
-        if (elem.store.openinghours.today.from == '' && elem.store.openinghours.today.to == '') {
-            var dayOfWeek = today.format("d");
-            var todayWeekdayName = getDayOfWeekConstant(dayOfWeek).toLowerCase();
-            var everydayFrom = '';
-            var everydayTo = '';
-
-            elem.store.openinghours.days.map(function(day) {
-                if (day.label.toLowerCase() == todayWeekdayName) {
-                    elem.store.openinghours.today.from = day.from;
-                    elem.store.openinghours.today.to = day.to;
-                    return;
-                }
-                if (day.label.toLowerCase() == "hverdager") {
-                    everydayFrom = day.from;
-                    everydayTo = day.to;
-                }
-            });
-
-            if (elem.store.openinghours.today.from == '' && elem.store.openinghours.today.to == '' && (parseInt(dayOfWeek) <= 5)) {
-                elem.store.openinghours.today.from = everydayFrom;
-                elem.store.openinghours.today.to = everydayTo;
-            }
-        }
-
-    });
-}
-
-function getDayOfWeekConstant(dayOfWeek) {
-    switch (parseInt(dayOfWeek)) {
-    case 1: return "Mandag";
-    case 2: return "Tirsdag";
-    case 3: return "Onsdag";
-    case 4: return "Torsdag";
-    case 5: return "Fredag";
-    case 6: return "Lørdag";
-    case 7: return "Søndag";
-    default: return "";
-    }
-}
-
-// CODE REVIEW PLEASE
-function getClosestStores(chainId, latitude, longitude, minNumberOfStores, maxNumberOfStores, maxDistance, filter, callback) {
-    var myPos = {"latitude": latitude, "longitude": longitude};
-    var FAR_FAR_AWAY = Math.pow(10, 100);
-
-    repository.getStores(chainId, function (stores) {
-        
-        if (stores.response.code != "200")
-            return [];
-
-        var storeArray = [];
-        if (stores.response.data) {
-            // TODO: more error checking here
-            storeArray = stores.response.data.map(function(elem) {
-                var distance,
-                    location = elem.location;
-
-                if (!location || !isNumber(location.latitude) || !isNumber(location.longitude)) {
-                    distance = FAR_FAR_AWAY;
-                } else {
-                    distance = geolib.getDistance(myPos, location) / 1000;
-                }
-
-                return createStoreDistanceObject(elem, distance);
-            });
-
-
-            storeArray.sort(function(a, b) {
-                return a.distance - b.distance;
-            });
-            storeArray = filterByOpeninghours(storeArray, filter);
-            storeArray = filterByLimits(storeArray, minNumberOfStores, maxNumberOfStores, maxDistance);
-            
-            var today = moment().tz("Europe/Oslo");
-            applyTodaysOpeningHours(today, storeArray);
-        }
-
-        callback(storeArray);
-    });
-}
-
-
 exports.getClosestStores = getClosestStores;
-exports.applyTodaysOpeningHours = applyTodaysOpeningHours;
 
 exports.setRepository = function(repo) {
     repository = repo;
--- spec/stores/closest-stores.test.js
+++ spec/stores/closest-stores.test.js
@@ -2,32 +2,37 @@ var expect = require('chai').expect;
 var _ = require('lodash');
 var storeService = require('../../modules/stores/store_service');
 
+
 describe('Store service', function () {
     var DELTA_IN_PERCENT = 0.5;
 
     this.timeout(200);
 
     describe('Find the closest stores', function () {
+        
+        
 
         var inMemRepository = {
-            getStores: function repository(cb) {
-                cb(require('./fixtures/kiwistores'));
-            }
+            getStores: 
+                function repository(chainId, cb) {
+                    cb(require('./fixtures/kiwistores'));
+                }
         };
 
         beforeEach(function () {
             storeService.setRepository(inMemRepository);
         });
 
-        function testCase(lat, long, maxdistance, filter, testCaseFileName, callback) {
+        function testCase(chainId, lat, lon, maxdistance, filter, testCaseFileName, callback) {
 
-            storeService.getClosestStores(lat, long, 1, 0, maxdistance, filter, function (actualResults) {
+            storeService.getClosestStores(chainId, lat, lon, 1, 0, maxdistance, filter, function (actualResults) {
                 var expectedResults = require(testCaseFileName);
 
                 for (var i = 0, actual, expected; i < actualResults.length; i++) {
+                    
                     actual = actualResults[i];
                     expected = expectedResults[i];
-
+                    
                     expect(actual.store).to.eql(expected.store);
                     expect(actual.distance / expected.distance * 100).to.be.closeTo(100, DELTA_IN_PERCENT);
                 }
@@ -37,32 +42,42 @@ describe('Store service', function () {
         }
 
         it('should pass test case 1 of jira case #43751-1147', function (done) {
-            testCase(59.9170013, 10.72788689999993, 500, null, './results/43751-1147_testcase1.json', done);
+            testCase(1100, 59.9170013, 10.72788689999993, 500, null, './expected/43751-1147_testcase1.json', done);
         });
 
         it('should pass test case 2 of jira case #43751-1147', function (done) {
-            testCase(63.4305149, 10.39505280000003, 1230297, 'isopensunday', './results/43751-1147_testcase2.json', done);
+            testCase(1100, 63.4305149, 10.39505280000003, 1230297, 'isopensunday', './expected/43751-1147_testcase2.json', done);
         });
 
-        it('should consider incomplete location data as being equal to being really far away', function () {
-            var storesWithNullLocation = [
+        it('should ignore stores with invalid location data', function () {
+            var storesWithNullLocation =
+            {
+                "response":
                 {
-                    "name": "Kiwi 104 Moholt",
-                    "location": {
-                        "latitude": 0,
-                        "longitude": 0
+                    "code": 200,
+                    "origin": "testcode",
+                    "data": [
+                        {
+                            
+                            "name": "Kiwi 104 Moholt",
+                            "location": {
+                                "latitude": 0,
+                                "longitude": 0
 
-                    }
-                },
-                {
-                    name: "Ny butikk med uferdige data",
-                    "location": {
-                        "latitude": null,
-                        "longitude": null
-                    }
+                            }
+                        },
+                        {
+                            "name": "Ny butikk med uferdige data",
+                            "location": {
+                                "latitude": null,
+                                "longitude": null
+                            }
+                        }
+                    ]
                 }
-            ], repoReturningStoresWithNullInLocation = {
-                getStores: function repository(cb) {
+            };
+            var repoReturningStoresWithNullInLocation = {
+                getStores: function repository(chainId, cb) {
                     cb(storesWithNullLocation);
                 }
             };
@@ -77,7 +92,6 @@ describe('Store service', function () {
                     expect(res).to.eql([{store: storesWithNullLocation[0], distance: 0}]);
                 }
             );
-
 
         });
 
