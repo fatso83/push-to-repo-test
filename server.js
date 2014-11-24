3818be2a63d86582ed87592e32815f1426c1329f
ronpal <pr@makingwaves.no>
2014-11-24 08:49:47 +0100

    redeploy, grasping

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(process.env.PORT | 1337);
-console.log('Server running');
+console.log('Server running');  
