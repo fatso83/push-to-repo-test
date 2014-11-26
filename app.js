97aa8caab28a29e87979ad7137115eb631e51081
ronpal <pr@makingwaves.no>
2014-11-26 15:18:01 +0100

    Deploying to production

--- app.js
+++ app.js
@@ -1,13 +1,102 @@
-var server,
-    config = require('./modules/configuration-loader'),
-    log4js = require('log4js');
+config = require('./modules/configuration-loader'),
+log4js = require('log4js');
 
-console.log('#####################')
-console.log('#####################')
-console.log(require, JSON.stringify(require))
-console.log('#####################')
-console.log('#####################')
 
-console.log('(require.main === module) ', (require.main === module));
-console.log('(require.main) ', require.main);
-console.log('(module) ', module);
+/**
+ * @param callback for integration testing
+ */
+function main(config, callback) {
+    
+    var express = require('express'),
+        compress = require('compression'),
+        cors = require('cors'),
+        logger = require('morgan'),
+        bodyParser = require('body-parser'),
+        cookieParser = require('cookie-parser'),
+        path = require('path'),
+        debug = require('debug')('ng-azure-rest-api');
+    
+    // Set global logging level
+    log4js.setGlobalLogLevel(log4js.levels[config.logging.level]);
+    
+    // Routes
+    var request = require('./routes/request');
+    var index = require('./routes/index');
+    
+    var app = express();
+    server = require('http').createServer(app);
+    
+    app.set('port', config.port);
+    
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+    
+    app.use(compress());
+    app.use(cors());
+    app.use(logger('dev'));
+    
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    
+    app.use(express.static(path.join(__dirname, 'public')));
+    
+    app.use('/', index);
+    app.use('/request', request);
+    app.use('/api/FindStore', require('./routes/find-store'));
+    
+    // Catch 404 and forwarding to error handler
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        
+        err.status = 404;
+        err.message = "That route does not exist";
+        
+        next(err);
+    });
+    
+    server.listen(app.get('port'), function () {
+        
+        // Warm up caches
+        if (!config.disable['cache-warmup']) {
+            var cacheWarmer = require('./modules/caching/boot-script');
+            cacheWarmer.start();
+        }
+        
+        if (!config.disable['product-module']) {
+            // Load data needed for the search module
+            var searchUtil = require('./modules/productSearch/searchUtil');
+            searchUtil.loadData(function () {
+                
+                // finally everything has been setup
+                if (callback) {
+                    callback();
+                }
+            });
+        } else { callback(); }
+
+    });
+}
+
+/**
+ * expose stop and start functions for testing
+ */
+
+exports.stop = function (callback) {
+    server.close();
+    server.on('close', callback);
+};
+
+exports.start = function (overrides, cb) {
+    config.load(overrides, function (config) {
+        main(config, cb);
+    });
+};
+
+/* start as normal if run directly from node */
+//if (require.main === module) {
+
+config.load(function (config) {
+    main(config, console.log.bind(console, 'Express server listening at :' + config.port));
+});
+//}
