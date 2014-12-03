ce38067765c41e58ebbde61c11ece285bbb4fed4
ronpal <pr@makingwaves.no>
2014-12-03 13:55:32 +0100

    Added script for setting environment variables in Windows

---
+++ SetDevEnvironmentVariablesWin32.cmd
@@ -0,0 +1,8 @@
+@Echo Off
+@Echo Setting Dev Environment Variables
+setlocal
+SET REDIS_URI=ngredisdev.redis.cache.windows.net
+SET REDIS_PORT=6379
+SET REDIS_KEY=5SziauARAm7mWus0zl3sfv4OwccMVEtwIh5vMgp/D7c=
+SET CONFIGURATION_PROFILE=development
+@Echo Done
