d6a84d8f8727da9094311a853ff594b849b5581a
Carl-Erik Kopseng <carlerik@gmail.com>
2014-12-04 13:56:28 +0100

    Simple test to see that redis does in fact overwrite its cache.

---
+++ spec/integration/redis-cache.test.js
@@ -0,0 +1,23 @@
+var redisCache = require('../../modules/caching/redis-cache');
+var expect = require('chai').expect;
+
+describe('slow.redis-cache', function () {
+
+    it('should be able to overwrite an existing key', function (done) {
+        var key = 'myKey';
+        redisCache.cache(key, 'val1', function (res) {
+            redisCache.get(key, function (res) {
+
+                expect(res.data).to.equal('val1');
+
+                redisCache.cache(key, 'val2', function (res) {
+                    redisCache.get(key, function (res) {
+                        expect(res.data).to.equal('val2');
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+});
